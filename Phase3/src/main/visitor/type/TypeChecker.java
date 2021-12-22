afab5807a8d2ee090c3d1534f41172a7d7f5b2a3
kalhorghazal <kalhorghazal1378@gmail.com>
2021-12-22 15:43:40 -0500

    Add some stmt type checking

--- Phase3/src/main/visitor/type/TypeChecker.java
+++ Phase3/src/main/visitor/type/TypeChecker.java
@@ -3,11 +3,21 @@ package main.visitor.type;
 import main.ast.nodes.Program;
 import main.ast.nodes.declaration.*;
 import main.ast.nodes.declaration.struct.*;
+import main.ast.nodes.expression.Expression;
+import main.ast.nodes.expression.Identifier;
+import main.ast.nodes.expression.ListAccessByIndex;
+import main.ast.nodes.expression.StructAccess;
+import main.ast.nodes.expression.operators.BinaryOperator;
 import main.ast.nodes.statement.*;
+import main.ast.types.*;
+import main.ast.types.primitives.BoolType;
+import main.ast.types.primitives.IntType;
+import main.compileError.typeError.*;
 import main.visitor.Visitor;
 
 public class TypeChecker extends Visitor<Void> {
     ExpressionTypeChecker expressionTypeChecker;
+    public FunctionDeclaration currentFunction;
 
     public void TypeChecker(){
         this.expressionTypeChecker = new ExpressionTypeChecker();
@@ -15,7 +25,12 @@ public class TypeChecker extends Visitor<Void> {
 
     @Override
     public Void visit(Program program) {
-        //Todo
+        //Todo: done:)
+        for (StructDeclaration structDeclaration: program.getStructs())
+            structDeclaration.accept(this);
+        for (FunctionDeclaration functionDeclaration:program.getFunctions())
+            functionDeclaration.accept(this);
+        program.getMain().accept(this);
         return null;
     }
 
@@ -51,61 +66,125 @@ public class TypeChecker extends Visitor<Void> {
 
     @Override
     public Void visit(AssignmentStmt assignmentStmt) {
-        //Todo
+        //Todo: done:)
+        Type firstType = assignmentStmt.getLValue().accept(expressionTypeChecker);
+        Type secondType = assignmentStmt.getRValue().accept(expressionTypeChecker);
+        Expression left = assignmentStmt.getLValue();
+        if(!(left instanceof Identifier || left instanceof StructAccess || left instanceof ListAccessByIndex)) {
+            LeftSideNotLvalue exception = new LeftSideNotLvalue(assignmentStmt.getLine());
+            assignmentStmt.addError(exception);
+        }
+        if(firstType instanceof NoType || secondType instanceof NoType) {
+            return null;
+        }
+        if(firstType instanceof IntType || firstType instanceof BoolType || firstType instanceof FptrType) {
+            if (firstType.toString().equals(secondType.toString())) {
+                return null;
+            }
+        }
+        if(firstType instanceof ListType && secondType instanceof ListType) {
+            if (((ListType) firstType).getType().equals(((ListType) secondType).getType()))
+                return null;
+        }
+        if((firstType instanceof StructType && secondType instanceof StructType)) {
+            if (((StructType) firstType).getStructName().getName().equals(((StructType) secondType).getStructName().getName())) {
+                return null;
+            }
+        }
+        UnsupportedOperandType exception = new UnsupportedOperandType(assignmentStmt.getLine(), BinaryOperator.assign.name());
+        assignmentStmt.addError(exception);
         return null;
     }
 
     @Override
     public Void visit(BlockStmt blockStmt) {
-        //Todo
+        //Todo: done:)
+        for(Statement statement : blockStmt.getStatements()) {
+            statement.accept(this);
+        }
         return null;
     }
 
     @Override
     public Void visit(ConditionalStmt conditionalStmt) {
-        //Todo
+        //Todo: done:)
+        Type condType = conditionalStmt.getCondition().accept(expressionTypeChecker);
+        if(!(condType instanceof BoolType || condType instanceof NoType)) {
+            ConditionNotBool exception = new ConditionNotBool(conditionalStmt.getLine());
+            conditionalStmt.addError(exception);
+        }
+        conditionalStmt.getThenBody().accept(this);
+        if(conditionalStmt.getElseBody() != null) {
+            conditionalStmt.getElseBody().accept(this);
+        }
         return null;
     }
 
     @Override
     public Void visit(FunctionCallStmt functionCallStmt) {
-        //Todo
+        //Todo: done:)
+        expressionTypeChecker.setIsInFuncCallStmt(true);
+        functionCallStmt.getFunctionCall().accept(expressionTypeChecker);
+        expressionTypeChecker.setIsInFuncCallStmt(false);
         return null;
     }
 
     @Override
     public Void visit(DisplayStmt displayStmt) {
-        //Todo
+        //Todo: done:)
+        Type argType = displayStmt.getArg().accept(expressionTypeChecker);
+        if(!(argType instanceof IntType || argType instanceof BoolType
+                || argType instanceof ListType || argType instanceof NoType)) {
+            UnsupportedTypeForDisplay exception = new UnsupportedTypeForDisplay(displayStmt.getLine());
+            displayStmt.addError(exception);
+        }
         return null;
     }
 
     @Override
     public Void visit(ReturnStmt returnStmt) {
-        //Todo
+        //Todo: done:)
+        Type retType = returnStmt.getReturnedExpr().accept(expressionTypeChecker);
+        Type actualRetType = this.currentFunction.getReturnType();
+        if(!retType.toString().equals(actualRetType.toString())) {
+            ReturnValueNotMatchFunctionReturnType exception = new ReturnValueNotMatchFunctionReturnType(returnStmt.getLine());
+            returnStmt.addError(exception);
+        }
         return null;
     }
 
     @Override
     public Void visit(LoopStmt loopStmt) {
-        //Todo
+        //Todo: done:)
+        Type condType = loopStmt.getCondition().accept(expressionTypeChecker);
+        if(!(condType instanceof BoolType || condType instanceof NoType)) {
+            ConditionNotBool exception = new ConditionNotBool(loopStmt.getLine());
+            loopStmt.addError(exception);
+        }
+        loopStmt.getBody().accept(this);
         return null;
     }
 
     @Override
     public Void visit(VarDecStmt varDecStmt) {
-        //Todo
+        //Todo: done:)
+        for(VariableDeclaration variableDeclaration: varDecStmt.getVars()) {
+            variableDeclaration.accept(this);
+        }
         return null;
     }
 
     @Override
     public Void visit(ListAppendStmt listAppendStmt) {
-        //Todo
+        //Todo: done:)
+        listAppendStmt.getListAppendExpr().accept(expressionTypeChecker);
         return null;
     }
 
     @Override
     public Void visit(ListSizeStmt listSizeStmt) {
-        //Todo
+        //Todo: done:)
+        listSizeStmt.getListSizeExpr().accept(expressionTypeChecker);
         return null;
     }
 }
